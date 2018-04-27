986dc9ba7e26a169d0221cff539ca839401f4c67
Jiri Zamecnik <jerry.zamecnik@gmail.com>
2018-04-27 11:17:49 +0200

    Intro slide
    Added an introduction to the interface which displays before the data is loaded. Some other small changes in the interface.

--- server.r
+++ server.r
@@ -51,10 +51,47 @@ shinyServer(function(input, output) {
 	vals <- reactiveValues(keeprows=TRUE, markcases=FALSE)
 	
     output$sum <- renderPrint({
-      summaryOutput <- summary(datasetInput())
-      if(length(summaryOutput)==3){invisible()} else {summaryOutput}
+		summaryOutput <- summary(datasetInput())
+		if(length(summaryOutput)==3){invisible()} 
+		else {summaryOutput}
       
     })
+	
+	output$help <- renderUI({
+		if (is.null(datasetInput()) == TRUE) {
+				tagList(h3("Welcome to FREDDIE Shiny!"),
+							h4("How to use it"),
+							HTML('FREDDIE Shiny is an interface to allow you simple quantitative analyses of your data. In order to use it, you will need a dataset in the CSV format.
+								For practice you can get some data <a href="https://1drv.ms/u/s!AiIxNMjRmvCLj-cpj0yUHcS9uDSLCQ">here</a>.'),
+							p("The interface loads after you uploaded the dataset. If it displays errors, make sure that you select the correct settings for 'Separator', 'Quote sign' and 'Header' with respect to the CSV file you uploaded."),
+							p("Once everything is ready, use the 'Data summary' tab to check that the individual variables were correctly recognized as categories or numbers."),
+							h4("Creating plots"),
+							p("Plots can be created in the tabs 'Variable summary' and 'Plot', depending whether you want to plot the distribution of one of your variables on its own or the relationship between your dependent and independent variables.
+								The plot type is selected automatically and depends on the type of variable used. Still you have some options to determine the exact visuals of most of the plots. The plots used include:"),
+							HTML('<ul>
+								<li><a href="https://en.wikipedia.org/wiki/Histogram">Histogram</a></li>
+								<li><a href="https://en.wikipedia.org/wiki/Box_plot">Boxplot</a></li>
+								<li><a href="https://en.wikipedia.org/wiki/Scatter_plot">Scatterplot</a></li>
+								<li><a href="https://en.wikipedia.org/wiki/Bar_chart">Bar chart</a></li>
+								</ul>'),
+							h4("Statistical testing"),
+							p("Statistical tests are automated as far as possible, so you should not make any grave mistakes. Still, some of them require some input from you. This is always described together with the test in the correspondig tab"),
+							h4("Procedure"),
+							HTML('<ol>
+								<li>Load your data in CSV format</li>
+								<li>Verify that FREDDIE imported the data correctly, if not adjust the necessary settings.</li>
+								<li>Choose your <b>independent (predicting) variable</b> - usually something like "age" or "gender"</li>
+								<li>Choose your <b>dependent (predicted) variable</b> - usually something like "points" or "frequency"</li>
+								<li>Explore the distribution of the variables on their own</li>
+								<li>Look at their relationship</li>
+								<li>If there are <a href="https://en.wikipedia.org/wiki/Outlier">outliers</a> in the data, use the outlier removal function to exclude them. <b> Only click the "Apply" button once!</b></li>
+								<li>Adjust the settings of the selected statistical test, if needed.</li>
+								<li>View the results of the test. Is there a significant relationship/difference?</li>
+								</ol>')
+				)
+			}
+		else {invisible()}
+		})
 	
     output$varselector <- renderUI({
       tagList(
@@ -113,54 +150,62 @@ shinyServer(function(input, output) {
 
     
 	output$PlotSettings <- renderUI({
-		if (PlotType()=="Bar plot:"){tagList(
-				checkboxInput("leg", "Legend", TRUE),
-				checkboxInput("besideCheck", "Do not stack", FALSE),
-				radioButtons("propCheck", "",	choices = c("absolute", "relative"), selected = "absolute")
-				)
-			}
-		else if (PlotType()=="Box plot:"){tagList(
-				checkboxInput("notchCheck", "Notches", FALSE)
-				)
-			}
-		else if (PlotType()=="Scatter plot:"){tagList(
-				checkboxInput("regrCheck", "Regression line", FALSE),
-				checkboxInput("regrSE", "Display standard error", FALSE)
-				)
-			}
-		 
-		else {invisible()}
+		if (is.null(datasetInput()) == TRUE) {invisible()}
+		else{
+			if (PlotType()=="Bar plot:"){tagList(
+					checkboxInput("leg", "Legend", TRUE),
+					checkboxInput("besideCheck", "Do not stack", FALSE),
+					radioButtons("propCheck", "",	choices = c("absolute", "relative"), selected = "absolute")
+					)
+				}
+			else if (PlotType()=="Box plot:"){tagList(
+					checkboxInput("notchCheck", "Notches", FALSE)
+					)
+				}
+			else if (PlotType()=="Scatter plot:"){tagList(
+					checkboxInput("regrCheck", "Regression line", FALSE),
+					checkboxInput("regrSE", "Display standard error", FALSE)
+					)
+				}
+			 
+			else {invisible()}
+		}
 	})	
 	
 	output$OutlierFilter <- renderUI({
-		if (PlotType()=="Scatter plot:") 
-				{tagList(
-					h3("Outlier removal"),
-					p("Mark outliers (cases that are too extreme to be realistic) by clicking 
-						or dragging a box and then clicking the 'Select' button. To remove them from the statistical tests, click 'Apply'."),
-					p("You should select ALL outliers before hitting the 'Apply' button. "),
-					fluidRow(
-						column(1, actionButton("exclude_toggle", "Select")),
-						column(1, actionButton("exclude_reset", "Reset")),
-						column(1, actionButton("apply_removal", "Apply", style="color: #fff; background-color: #009933;")),
-						column(9, invisible())
+	if (is.null(datasetInput()) == TRUE) {invisible()}
+	else {
+			if (PlotType()=="Scatter plot:") 
+					{tagList(
+						h3("Outlier removal"),
+						p("Mark outliers (cases that are too extreme to be realistic) by clicking 
+							or dragging a box and then clicking the 'Select' button. To remove them from the statistical tests, click 'Apply'."),
+						p("You should select ALL outliers before hitting the 'Apply' button. "),
+						fluidRow(
+							column(1, actionButton("exclude_toggle", "Select")),
+							column(1, actionButton("exclude_reset", "Reset")),
+							column(1, actionButton("apply_removal", "Apply", style="color: #fff; background-color: #009933;")),
+							column(9, invisible())
+							)
 						)
-					)
-				}	
-		else if (PlotType()=="Box plot:") 
-				{tagList(
-					h3("Outlier removal"),
-					p("Mark outliers (cases that are too extreme to be realistic) by clicking 
-						or dragging a box and then clicking the 'Select' button.  To remove them from the statistical tests, click 'Apply'."),
-					p("You should select ALL outliers before hitting the 'Apply' button. If the redrawn chart shows new outlier-like cases, do not remove them anymore."),
-					fluidRow(
-						column(1, actionButton("exclude_toggle", "Select")),
-						column(1, actionButton("exclude_reset", "Reset")),
-						column(1, actionButton("apply_removal", "Apply", style="color: #fff; background-color: #009933;")),
-						column(9, invisible())
+					}	
+			else if (PlotType()=="Box plot:") 
+					{tagList(
+						h3("Outlier removal"),
+						p("Mark outliers (cases that are too extreme to be realistic) by clicking 
+							or dragging a box and then clicking the 'Select' button.  To remove them from the statistical tests, click 'Apply'."),
+						p("You should select ALL outliers before hitting the 'Apply' button. If the redrawn chart shows new outlier-like cases, do not remove them anymore."),
+						fluidRow(
+							column(1, actionButton("exclude_toggle", "Select")),
+							column(1, actionButton("exclude_reset", "Reset")),
+							column(1, actionButton("apply_removal", "Apply", style="color: #fff; background-color: #009933;")),
+							column(9, invisible())
+							)
 						)
-					)
-				}		
+					}
+			else {invisible()}
+			}
+		
 		})
 		
     # dataobject <- reactive({
@@ -218,8 +263,8 @@ shinyServer(function(input, output) {
         if (PlotType()=="Bar plot:"){
 			if (input$propCheck=="absolute"){
 				outPlot <- ggplot(datasetInput(), aes(datasetInput()[[input$xvar]], fill=datasetInput()[[input$yvar]])) +
-					scale_x_discrete(name=input$xvar) + 
-					scale_fill_discrete(name = input$yvar) +
+					scale_x_discrete(name=XTitle()) + 
+					scale_fill_discrete(name = YTitle()) +
 					ggtitle(input$titleInput)+
 					theme_bw()+
 					scale_fill_grey(start=0, end=0.9) +
@@ -234,15 +279,33 @@ shinyServer(function(input, output) {
 					}	
 				else {outPlot <- outPlot + theme(legend.position="none")}}
 			else {
-				datatabletemp <- prop.table(table(datasetInput()[[input$yvar]], datasetInput()[[input$xvar]]), 2)
-				outPlot <- barplot(datatabletemp, beside=input$besideCheck, col=gray.colors(length(levels(datasetInput()[[input$yvar]])), start = 0.0, end = 0.9))
+				outPlot <- ggplot(datasetInput(), aes(datasetInput()[[input$xvar]], fill=datasetInput()[[input$yvar]])) +
+					scale_x_discrete(name=XTitle()) + 
+					scale_fill_discrete(name = YTitle()) +
+					ggtitle(input$titleInput) +
+					theme_bw()+
+					scale_fill_grey(start=0, end=0.9) +
+					theme(plot.title = element_text(hjust=0.5, face="bold", size=14))
+				
+				if (input$besideCheck==TRUE){
+					outPlot <- outPlot + geom_bar(position=position_dodge())
+					}	
+				else {outPlot <- outPlot + geom_bar(position="fill")}
+				if (input$leg==TRUE){
+					outPlot <- outPlot + theme(legend.position="right")
+					}	
+				else {outPlot <- outPlot + theme(legend.position="none")}}
+			
+			
+				# datatabletemp <- prop.table(table(datasetInput()[[input$yvar]], datasetInput()[[input$xvar]]), 2)
+				# outPlot <- barplot(datatabletemp, beside=input$besideCheck, col=gray.colors(length(levels(datasetInput()[[input$yvar]])), start = 0.0, end = 0.9))
 				}
 			
 			return(outPlot)
 			
         }
       }
-    }
+   
     
 	# Toggle points that are clicked
 	observeEvent(input$plot1_click, {
